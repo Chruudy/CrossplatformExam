96dd17653e3e683d1d7c633122b1ccd6f2894a64
Christian Ruud <christianr74@live.com>
2024-11-21 13:15:05 +0100

    domain-mixup fix

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -15,10 +15,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyA1oDPqeL_hL2KMrw3uKWnmLfY7RFHeJHg",
   authDomain: "artvista-2c1e0.firebaseapp.com",
   projectId: "artvista-2c1e0",
-  storageBucket: "artvista-2c1e0.appspot.com",
+  storageBucket: "artvista-2c1e0.firebasestorage.app",
   messagingSenderId: "718463587029",
   appId: "1:718463587029:web:afaee1edc20f43e56d085c",
-  measurementId: "G-BN3LTMBFCD",
+  measurementId: "G-BN3LTMBFCD"
 };
 
 const app = initializeApp(firebaseConfig);
